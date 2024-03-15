1c2b4553d8898a78916fe589cb3d0a5b811a3fc5
Temweka Chirwa <47864157+Temweka@users.noreply.github.com>
2024-03-15 14:02:24 +0200

    change units to metric

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 def get_current_weather(city="Kansas City"):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 
     weather_data = requests.get(request_url).json()
 
